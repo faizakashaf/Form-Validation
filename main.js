4a167923b6eef5866f7bf68fa60e1acc40acaae2
Faiza Kashaf <faizakashaf4@gmail.com>
2024-01-23 22:47:15 +0500

    added some phone validation and confirm password validation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//calling all inputs 
 const form = document.querySelector("#form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
@@ -5,8 +6,9 @@ const phone = document.querySelector("#phone");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPass");
 
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+e.preventDefault()
 validateInput()
 
 const hasErrors = 
@@ -17,22 +19,36 @@ password.parentElement.querySelector(".error").innerText !== "" ||
 confirmPassword.parentElement.querySelector(".error").innerText !== "";
 
 if(!hasErrors){
-    window.location.href = "formSubmission.html";
+    setTimeout(()=>{
+        window.location.href = "formSubmission.html";
+    },1000)
     }
 });
 
+//errors messege for every element
 const  setErrors = (element,message) => {
 const inputControl = element.parentElement;
 const span = inputControl.querySelector(".error");
 span.innerText = message;
-
+const input = inputControl.querySelector("input");
+input.classList.add("red");
+input.classList.remove("green");
 }
 
+//after settling errors remove error
 const clearErrors = (element) =>{
     const inputControl = element.parentElement;
     const span = inputControl.querySelector(".error");
     span.innerText = "";
+    const input = inputControl.querySelector("input");
+    input.classList.remove("red");
+   input.classList.add("green");
 }
+
+//phone number validation that user cant input any string
+phone.addEventListener('input', function() {
+    this.value = this.value.replace(/[^0-9]/g, '');
+  });
 
 function startsWithNumber(userNameValue) {
     return /^\d/.test(userNameValue);
@@ -45,85 +61,59 @@ const phoneValue = phone.value.trim();
 const passwordValue = password.value.trim();
 const cpassValue = confirmPassword.value.trim();
 
-
 //username validation
 if(userNameValue === ""){
     setErrors(username,"* Username can not be empty");
-   
-
 }
 
 else if(userNameValue.length >15){
     setErrors(username,"* Username is too long and not valid");
- 
-
 }
 else if(userNameValue.length < 8){
     setErrors(username,"* Username is too short");
-   
-
 }
 else if(startsWithNumber(userNameValue)){
     setErrors(username,"* Username can not start with a digits");
-
-
 }
 else{
-    clearErrors(username);
-   
+    clearErrors(username);  
 }
 
 //email validation
 if(emailValue === ""){
     setErrors(email,"* Email can not be empty");
-   
-
 }
 else if(emailValue.length > 30){
     setErrors(email,"* Email is too long");
-   
-
 }
 else if (isEmail(emailValue) === false){
-    setErrors(email,"* Email should be in required format")
-    
+    setErrors(email,"* Email should be in required format")   
 }
 else{
     clearErrors(email)
-
 }
 
 //phone validation
 if(phoneValue === ""){
-setErrors(phone,"* Phone No can not be empty")
-
-
+setErrors(phone,"* Phone number can not be empty")
 }
-else if(phoneValue.length < 11 || phoneValue.length > 11){
-    setErrors(phone,"* Phone No must be of eleven digits")
- 
 
+else if(phoneValue.length < 11 || phoneValue.length > 11){
+    setErrors(phone,"* Phone number must be of eleven digits")
 }
 else{
     clearErrors(phone)
-
 }
 //password validation
 
 if(passwordValue === ""){
     setErrors(password,"* Password can not be empty");
-  
-
 }
-else if(passwordValue.length < 8 || passwordValue.length >15){
+else if(passwordValue.length < 8 || passwordValue.length >20){
 setErrors(password,"* min-length: 8 and max-length: 15")
-
-
 }
 else if(isPassword(passwordValue) === false){
     setErrors(password,"* Password should be alphanumeric containing atleast 1 uppercase and 1 special character")
- 
-
 }
 else{
     clearErrors(password)
@@ -132,11 +122,12 @@ else{
 //confirm password validation
 if(cpassValue === ""){
     setErrors(confirmPassword,"* Password can not be empty");
- 
+}
+else if(isPassword(cpassValue) === false){
+    setErrors(confirmPassword,"* Password should be alphanumeric containing atleast 1 uppercase and 1 special character")
 }
 else if(cpassValue !== passwordValue){
-    setErrors(confirmPassword,"* Password does not match with the new password");
-   
+    setErrors(confirmPassword,"* Password does not match with the new password"); 
 }
 else{
     clearErrors(confirmPassword)
@@ -144,18 +135,18 @@ else{
 
 };
 
-
-
-
 function isEmail(emailValue){
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
    return emailPattern.test(emailValue);
 }
 
-function isPassword(passwordValue){
+function isPassword(passwordValue,cpassValue){
     const passwordPattern = /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
-    return passwordPattern.test(passwordValue);
+    return passwordPattern.test(passwordValue,cpassValue);
 }
+
+
+
 
 
 
